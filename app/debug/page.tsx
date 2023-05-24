205a1082ca5c6f20ecc220e73a3e6e07b5b41b0a
Tony Xiao <tonyx.ca@gmail.com>
2023-05-24 13:24:22 +0200

    networkMode tweaking

--- app/debug/page.tsx
+++ app/debug/page.tsx
@@ -7,16 +7,18 @@ import {
 } from '@tanstack/react-query'
 import React from 'react'
 
-function PageContent() {
-  const res = useQuery(['test'], () => ({ mynumber: Math.random() }), {
-    staleTime: Number.POSITIVE_INFINITY,
+function PageContent({ q }: { q: string }) {
+  const res = useQuery(['test', q], () => ({ mynumber: Math.random() }), {
+    // staleTime: Number.POSITIVE_INFINITY,
+    // staleTime: 0,
+    networkMode: 'always',
   })
   ;(globalThis as any).res = res
   return (
     <div className="space-y-8">
       {res.status}
       <h1 className="text-xl font-medium text-gray-300">
-        {res.data?.mynumber}
+        {q}: {res.data?.mynumber}
       </h1>
     </div>
   )
@@ -27,17 +29,23 @@ export default function Page() {
 
   const queryClient = React.useMemo(() => {
     console.log('Creating new query client')
-    return new QueryClient()
+    const client = new QueryClient()
+    Object.assign(client, { ts: new Date().toISOString() })
+    return client
   }, [slug])
 
   ;(globalThis as any).queryClient = queryClient
+
+  const [queryKey, setKey] = React.useState('tab1')
 
   return (
     <div className="space-y-8">
       <h1 className="text-xl font-medium text-gray-300">Hello {slug}</h1>
       Slug: <input value={slug} onChange={(e) => setSlug(e.target.value)} />
+      Query Key:{' '}
+      <input value={queryKey} onChange={(e) => setKey(e.target.value)} />
       <QueryClientProvider client={queryClient}>
-        <PageContent />
+        <PageContent q={queryKey} />
       </QueryClientProvider>
     </div>
   )
